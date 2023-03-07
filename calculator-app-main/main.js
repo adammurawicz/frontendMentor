1e36ccc5e60389d7f6454ceef1f2ac0f4b7737d3
adammurawicz <murawicz_adam@gmail.com>
2023-03-07 19:59:37 +0100

    end of calc project

--- calculator-app-main/main.js
+++ calculator-app-main/main.js
@@ -1,4 +1,4 @@
-const btns = document.querySelectorAll('.numberBtn')
+const numberBtns = document.querySelectorAll('.numberBtn')
 const sectionMiddle__p = document.querySelector('.sectionMiddle__p')
 const error = document.querySelector('.error')
 const memoNumber = document.querySelector('.sectionMiddle__memo')
@@ -11,11 +11,79 @@ const btnMinus = document.querySelector('.btnMinus')
 const btnMultiply = document.querySelector('.btnMultiply')
 const btnDivide = document.querySelector('.btnDivide')
 const actionBtns = [btnPlus, btnMinus, btnMultiply, btnDivide]
-
 const equalBtn = document.querySelector('.btnEqual')
-let num1; 
-let num2;
 
+const root = document.documentElement
+const toggleInputOne = document.querySelector('.toggleInputOne')
+const toggleInputTwo = document.querySelector('.toggleInputTwo')
+const toggleInputThree = document.querySelector('.toggleInputThree')
+const toggles = [toggleInputOne, toggleInputTwo, toggleInputThree]
+
+
+const checkToggle = () => {
+    if(toggleInputOne.checked) {
+        root.style.setProperty('--background1', 'hsl(222, 26%, 31%)');
+        root.style.setProperty('--background2', 'hsl(223, 31%, 20%)');
+        root.style.setProperty('--background3', 'hsl(224, 36%, 15%)');
+        root.style.setProperty('key1', 'hsl(225, 21%, 49%)');
+        root.style.setProperty('key2', 'hsl(224, 28%, 35%)');
+        root.style.setProperty('key3', 'hsl(6, 63%, 50%)');
+        root.style.setProperty('key4', 'hsl(6, 70%, 34%)');
+        root.style.setProperty('key5', 'hsl(30, 25%, 89%)');
+        root.style.setProperty('key6', 'hsl(28, 16%, 65%)');
+        root.style.setProperty ('text1', 'hsl(221, 14%, 31%)');
+        root.style.setProperty ('text2', 'hsl(0, 0%, 100%)');
+    } else if ( toggleInputTwo.checked) {
+        root.style.setProperty('--background1', 'hsl(0, 0%, 90%)');
+        root.style.setProperty('--background2', 'hsl(0, 5%, 81%)');
+        root.style.setProperty('--background3', 'hsl(0, 0%, 93%)');
+        root.style.setProperty('key1', 'hsl(185, 42%, 37%)');
+        root.style.setProperty('key2', 'hsl(185, 58%, 25%)');
+        root.style.setProperty('key3', 'hsl(25, 98%, 40%)');
+        root.style.setProperty('key4', 'hsl(25, 99%, 27%)');
+        root.style.setProperty('key5', 'hsl(45, 7%, 89%)');
+        root.style.setProperty('key6', 'hsl(45, 7%, 89%)');
+        root.style.setProperty ('text1', 'hsl(60, 10%, 19%)');
+        root.style.setProperty ('text2', 'hsl(0, 0%, 100%)');
+    } else if (toggleInputThree.checked) {
+        root.style.setProperty('--background1', 'hsl(268, 75%, 9%)');
+        root.style.setProperty('--background2', 'hsl(285, 91%, 52%)');
+        root.style.setProperty('--background3', 'hsl(268, 75%, 9%)');
+        root.style.setProperty('key1', 'hsl(281, 89%, 26%)');
+        root.style.setProperty('key2', 'hsl(285, 91%, 52%)');
+        root.style.setProperty('key3', 'hsl(176, 100%, 44%)');
+        root.style.setProperty('key4', 'hsl(177, 92%, 70%)');
+        root.style.setProperty('key5', 'hsl(268, 47%, 21%)');
+        root.style.setProperty('key6', 'hsl(290, 70%, 36%)');
+        root.style.setProperty ('text1', 'hsl(52, 100%, 62%)');
+        root.style.setProperty ('text2', 'hsl(198, 20%, 13%)');
+    }
+}
+
+
+// errors:
+const errorAction = (errorText) => {
+    error.textContent = errorText
+    setTimeout(()=> {
+        error.textContent = ""
+    }, 2500)
+}
+
+// divide on 0
+
+const zeroDivide = () => {
+    memoNumber.textContent = '';
+    sectionMiddle__p.textContent = ''
+    errorAction("Hey sucker! You can't divide on 0!")
+}
+
+// counting
+
+const countingAction = (x, operator, y) => {
+    console.log(x, operator , y);
+    sectionMiddle__p.textContent = eval(x + operator + y)
+    memoNumber.textContent = ''
+}
 
 
 // typing in paragraph
@@ -24,17 +92,12 @@ const typeAction = (e) => {
 
     if(sectionMiddle__p.textContent === '') {
         sectionMiddle__p.textContent = e.target.value
-        // num1 = parseFloat(sectionMiddle__p.textContent)
         
     } else {
         if (sectionMiddle__p.textContent.length > maxNumber) {
-            error.textContent = 'error of calculator - to long number for its precessor'
-            setTimeout(()=> {
-                error.textContent = ""
-            }, 2000)
+            errorAction('error of calculator - to long number for its precessor')
         } else {
             sectionMiddle__p.textContent = sectionMiddle__p.textContent + e.target.value
-            // num1 = parseFloat(sectionMiddle__p.textContent)
         }
     }
 }
@@ -53,7 +116,7 @@ const resetAction = () => {
 }
 
 
-
+// using '+', '-', '*', '/' btns:
 const mainAction = (e) => {
     let operator
 
@@ -62,101 +125,44 @@ const mainAction = (e) => {
         memoNumber.textContent = sectionMiddle__p.textContent + e.target.value
         sectionMiddle__p.textContent = ''
     } 
-    // divide on 0
-    else if (memoNumber.textContent.charAt(memoNumber.textContent.length - 1) == '/' && sectionMiddle__p.textContent == 0) {
-        memoNumber.textContent = '';
-        sectionMiddle__p.textContent = ''
-        error.textContent = "Hey sucker! You can't divide on 0!"
-        setTimeout(()=> {
-            error.textContent = ""
-        }, 2000)
-    } 
-    // typed memoNumber and typed sectionMiddle__p
-    else if (memoNumber.textContent != '' && sectionMiddle__p.textContent != '') {
-        num1 = parseFloat(memoNumber.textContent)
-        num2 = parseFloat(sectionMiddle__p.textContent)
-        operator = memoNumber.textContent.charAt(memoNumber.textContent.length - 1)
-        console.log(num1, operator ,num2);
-        sectionMiddle__p.textContent = eval(num1 + operator + num2)
-        memoNumber.textContent = ''
+    // 2.start with operator 
+    else if (memoNumber.textContent === '' && e.target.value != '') {
+        errorAction("don't start calculation with an operator")
     }
-    // change of operator
-   
+
+    // 3.divide on 0
+    else if (memoNumber.textContent.charAt(memoNumber.textContent.length - 1) == '/' && sectionMiddle__p.textContent == 0) {
+        zeroDivide()
+    } 
+    // 4.change of operator
+    else if (memoNumber.textContent.charAt(memoNumber.textContent.length - 1) != e.target.value){
+        memoNumber.textContent = memoNumber.textContent.replace(memoNumber.textContent.slice(memoNumber.textContent.length-1), e.target.value);
+    }
+    // 5.typed memoNumber and typed sectionMiddle__p
+    else if (memoNumber.textContent != '' && sectionMiddle__p.textContent != '') {
+        countingAction(parseFloat(memoNumber.textContent), memoNumber.textContent.charAt(memoNumber.textContent.length - 1), 
+        parseFloat(sectionMiddle__p.textContent))
+    } 
 }
 
-const check = () => {
-    console.log(memoNumber.textContent.charAt(memoNumber.textContent.length - 1));
-    console.log(sectionMiddle__p.textContent);
+const equalAction = () => {
+    if (memoNumber.textContent.charAt(memoNumber.textContent.length - 1) == '/' && sectionMiddle__p.textContent == 0){
+        zeroDivide()
+    } else if (
+        memoNumber.textContent != '' && sectionMiddle__p.textContent != ''
+    ) {
+        countingAction(parseFloat(memoNumber.textContent), memoNumber.textContent.charAt(memoNumber.textContent.length - 1), 
+        parseFloat(sectionMiddle__p.textContent))
+    }
 }
 
-equalBtn.addEventListener('click', check)
-// const plusAction = () => {
-
-//     if(memoNumber.textContent === '' && sectionMiddle__p.textContent != '') {
-//         memoNumber.textContent = parseFloat(sectionMiddle__p.textContent) + '+'
-//         sectionMiddle__p.textContent = ''
-//     } 
-
-//     else if (
-//         sectionMiddle__p.textContent == ''
-//     ) {
-//         console.log('no number');
-//         console.log(memoNumber.textContent.slice(-1));
-//         memoNumber.textContent = memoNumber.textContent.slice(0, memoNumber.textContent.length -1) + '+'
-//     }
-    
-//     else if (memoNumber.textContent.slice(-1) === '-' ||memoNumber.textContent.slice(-1) === '*' || memoNumber.textContent.slice(-1) === '/' )
-//     {
-//         console.log('change sign');
-//         console.log(memoNumber.textContent.slice(-1));
-//         memoNumber.textContent = memoNumber.textContent.slice(0, memoNumber.textContent.length -1) + '+'
-//     }
-    
-    
-//     else {
-//         if (sectionMiddle__p.textContent === '') {
-//             // console.log('error nic');
-//         } else {
-//             num1 = parseFloat(memoNumber.textContent)
-//             num2 = parseFloat(sectionMiddle__p.textContent)
-//             memoNumber.textContent = ''
-//             sectionMiddle__p.textContent = num1 + num2
-//         }
-//     }
-// }
-
-// const minusAction = () => {
-//     if(memoNumber.textContent === '') {
-//         memoNumber.textContent = parseFloat(sectionMiddle__p.textContent) + '-'
-//         sectionMiddle__p.textContent = ''
-//     } else {
-
-//         if (sectionMiddle__p.textContent === '') {
-//             // console.log('error nic');
-//         } else {
-//             num1 = parseFloat(memoNumber.textContent)
-//             num2 = parseFloat(sectionMiddle__p.textContent)
-//             memoNumber.textContent = ''
-//             sectionMiddle__p.textContent = num1 - num2
-//         }
-//     }
-// }
-
-
-// const equalAction = () => {
-//     if (memoNumber.textContent.slice(-1) === '+') {
-//         sectionMiddle__p.textContent = parseFloat(memoNumber.textContent) + parseFloat(sectionMiddle__p.textContent)
-//         memoNumber.textContent = ''
-//     } else if (memoNumber.textContent.slice(-1) === '-') {
-//         sectionMiddle__p.textContent = parseFloat(memoNumber.textContent) - parseFloat(sectionMiddle__p.textContent)
-//     }
-// }
 
 
 
-
-
-btns.forEach(btn => {
+toggles.forEach(toggle => {
+    toggle.addEventListener('click', checkToggle)
+})
+numberBtns.forEach(btn => {
     btn.addEventListener('click', typeAction)
 })
 btnDel.addEventListener('click', deleteAction)
@@ -164,8 +170,4 @@ btnReset.addEventListener('click', resetAction)
 actionBtns.forEach(btn => {
     btn.addEventListener('click', mainAction)
 })
-
-
-btnPlus.addEventListener('click', mainAction)
-// btnMinus.addEventListener('click', minusAction)
-// equalBtn.addEventListener('click', equalAction)
+equalBtn.addEventListener('click', equalAction)
